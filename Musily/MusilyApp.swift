3ecdc2ee478237f652c064bfd34d39295aaafe24
Lucas Flores <lucasflores@Lucas-Flores.local>
2023-06-21 08:58:16 -0300

    first commit

---
+++ Musily/MusilyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusilyApp.swift
+//  Musily
+//
+//  Created by Lucas Flores on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MusilyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
