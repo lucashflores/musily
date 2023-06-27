32f8baebd7a790fdb99cefe281cc9111d01f9147
lucashflores <lucas.henrique.flores2001@gmail.com>
2023-06-27 10:12:23 -0300

    changed playlist

--- Musily/MusicGetter.swift
+++ Musily/MusicGetter.swift
@@ -23,15 +23,15 @@ class MusicGetter: ObservableObject {
             switch status {
             case .authorized:
                 do {
-                    let request = MusicCatalogResourceRequest<Playlist>(matching: \.id, equalTo: MusicItemID(rawValue: "pl.u-GgA5kl6coX9rGYW"))
+                    let request = MusicCatalogResourceRequest<Playlist>(matching: \.id, equalTo: MusicItemID(rawValue: "pl.ba2404fbc4464b8ba2d60399189cf24e"))
                     let plResponse = try await request.response()
                     let playlist = plResponse.items.first
                     let plWithTracks = try await playlist?.with([.tracks])
                     let today = Date.now
                     let dateFormatter = DateFormatter()
                     dateFormatter.dateFormat = "dd"
-                    let day: Int = Int(dateFormatter.string(from: today)) ?? 8
-                    let songId = plWithTracks?.tracks?[day - 8].id
+                    let day: Int = Int(dateFormatter.string(from: today)) ?? 1
+                    let songId = plWithTracks?.tracks?[day - 1].id
                     let songRequest = MusicCatalogResourceRequest<Song>(matching: \.id, equalTo: songId ?? MusicItemID(rawValue: ""))
                     let songResponse = try await songRequest.response()
                     self.song = try await songResponse.items.first?.with([.artists])
